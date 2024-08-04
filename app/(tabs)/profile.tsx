4a3d2e3f4dc59ed55c8cb76fb5584915b0f7611b
John Kyle Salaysay <foursalaysay@gmail.com>
2024-08-04 23:42:17 +0800

    adding types and models

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,29 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Float } from 'react-native/Libraries/Types/CodegenTypes'
+
+interface PersonType {
+  name : String,
+  age : Float
+}
+
+
+
+const person : PersonType = 
+  {
+      name : "John Kyle",
+      age : 23
+  };
 
 const profile = () => {
+
+  const {name, age }= person;
+
   return (
     <View>
       <Text>profile</Text>
+      <Text>{name}</Text>
+      <Text></Text>
     </View>
   )
 }
--- app/(tabs)/transaction.tsx
+++ app/(tabs)/transaction.tsx
@@ -1,7 +1,21 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Double } from 'react-native/Libraries/Types/CodegenTypes'
+
+
+interface transactionInfo {
+  transactionId : String,
+  sender : String,
+  receiver : String,
+  amount : Double,
+  terms? : Int16Array,
+  balance: Int16Array,
+
+}
 
 const transaction = () => {
+
+
   return (
     <View>
       <Text>transaction</Text>
